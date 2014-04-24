05e2481d1625f1308d51b0fa390ee14dba3758e0
szymondrag <szymondrag@gmail.com>
2014-04-24 22:44:25 +0200

    Added Identity and Bind methods. Some basic usages added.

--- MonadsPlayground/Program.cs
+++ MonadsPlayground/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace MonadsPlayground
 {
-    class Program
+
+    public static class EXT
     {
-        static void Main(string[] args)
+        public static Identity<T2> Bind<T1, T2>(this Identity<T1> identity, Func<T1, Identity<T2>> func)
         {
+            return func(identity.Value);
+        }
+
+        public static Identity<T> ToIdentity<T>(this T value)
+        {
+            return new Identity<T>(value);
         }
     }
-}
+
+    public class Identity<T>
+    {
+        public T Value { get; private set; }
+        public Identity(T value)
+        {
+            Value = value;
+        }
+    }
+
+    class Program
+    {
+        void Main()
+        {
+            Func<int, Identity<int>> add2 = x => new Identity<int>(x + 2);
+            Func<int, Identity<int>> mul2 = x => new Identity<int>(x * 2);
+
+            Func<int, Identity<int>> add2mult2 = x => add2(x).Bind(mul2);
+
+            var result = "Hello World!".ToIdentity().Bind(x =>
+                            7.ToIdentity().Bind(y =>
+                                (new DateTime(2013, 1, 11)).ToIdentity().Bind(z =>
+                                (x + y.ToString() + z.ToShortDateString()).ToIdentity())));
+        }
+       
+
+      
+    }
+    }
